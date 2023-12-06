794d0456e14f06fd3de58ce9183699b3a70dbf72
DK <laancer4@gmail.com>
2023-12-06 14:12:37 +0900

    feat : 1장 - 객체, 설계

--- src/chapter_one/Theater.java
+++ src/chapter_one/Theater.java
@@ -11,6 +11,7 @@ public class Theater {
      * 초기 버전. 쓸모없는 중복이 다수 보인다
      * @param audience 관중
      */
+    // TODO: 2023-12-06 이 매서드를 리팩토링 해보기 (다른 클래스 또한 무제한으로 변경하거나 확장해도 무방함)
     public void enter(Audience audience){
         if (audience.getBag().hasInvitation()){
             Ticket ticket = ticketSeller.getTicketOffice().getTicket();
@@ -18,6 +19,8 @@ public class Theater {
             return;
         }
         Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+        // TODO: 2023-12-06 이 부분에서 ticket.getFee() 가 아닌 ticket.getCreditCard().pay() 의 형식으로 매서드가 바뀐다면?
+        // TODO: 변경에 유연한 코드를 만들기 위해서는 어떻게 해야할까?
         audience.getBag().minusAmount(ticket.getFee());
         ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
         audience.getBag().setTicket(ticket);
